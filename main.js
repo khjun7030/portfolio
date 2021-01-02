bf5e381536c7086fba11d20dcd2c9433f131b959
khjun7030 <76765318+khjun7030@users.noreply.github.com>
2021-01-02 20:41:24 +0900

    JS 링크

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ document.addEventListener('scroll', () => {
   }
 
   });
+
+  // Handle Scrolling when tapping on the navbar menu
+
+  const navbarMenu = document.querySelector('.navbar__menu');
+  navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+      return;
+    }
+
+    scrollIntoView(link);
+    //console.log(event.target.dataset.link);
+    //const scrollTo = document.querySelector(link);
+    //scrollTo.scrollIntoView({behavior:'smooth'});
+  });
+
+  // Handle click on "contact me" button on home
+  const homeContact = document.querySelector('.home__contact');
+  homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+  });
+  
+  function scrollIntoView(selector) { 
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth'});
+  }
